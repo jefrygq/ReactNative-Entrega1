b9425011d3728914e75a17216dd40862fdeb425e
jefrygq <69257961+jefrygq@users.noreply.github.com>
2023-05-23 19:42:18 -0600

    Removed unused AddScreen screen.

--- src/Screens/AddScreen/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-import Colors from '../../constants/colors';
-
-export default styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: Colors.background,
-	}
-});
